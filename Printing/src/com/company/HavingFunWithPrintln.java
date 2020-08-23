4decd357bdd8359498e391e1a7c34b4448b8c070
kwame <yamoahkwame@gmail.com>
2020-08-23 14:23:21 +0100

    Refreshing Java Print Knowledge
    Refreshed print knowledge and had fun printing ascii art

---
+++ Printing/src/com/company/HavingFunWithPrintln.java
@@ -0,0 +1,51 @@
+package com.company;
+
+public class HavingFunWithPrintln {
+    public static void main(String[] args) {
+        System.out.println(".....................TREE..................");
+        System.out.println("...........................................");
+        System.out.println("......................xxx..................");
+        System.out.println("...................xxxxxxxxx...............");
+        System.out.println("..................xxxxxxxxxxx..............");
+        System.out.println(".................xxxxxxxxxxxxx.............");
+        System.out.println("................xxxxxxxxxxxxxxx............");
+        System.out.println(".................xxxxxxxxxxxxx.............");
+        System.out.println("..................xxxxxxxxxxx..............");
+        System.out.println("......................xxx..................");
+        System.out.println("......................xxx..................");
+        System.out.println("......................xxx..................");
+        System.out.println("......................xxx..................");
+        System.out.println("......................xxx..................");
+        System.out.println();
+        System.out.println();
+
+
+        System.out.println(".....................APPLE..................................................................");
+        System.out.println("............................................................................................");
+        System.out.println("........................................x...................................................");
+        System.out.println(".......................................xxx....xx............................................");
+        System.out.println(".......................................xxxx..xxx............................................");
+        System.out.println("........................................xxx..xx.............................................");
+        System.out.println("..........................................xxx...............................................");
+        System.out.println(".....................xxxx.................xxx........xxx....................................");
+        System.out.println("................xxxxxxxxxxxxx..........xxx.....xxxxxxxxxxxxxx...............................");
+        System.out.println("..............xxxxxxxxxxxxxxxxx......xxx.....xxxxxxxxxxxxxxxxxx.............................");
+        System.out.println("............xxxxxxxxxxxxxxxxxxxxx...xxx...xxxxxxxxxxxxxxxxxxxxxxx...........................");
+        System.out.println("...........xxxxxxxxxxxxxxxxxxxxxxx.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx..........................");
+        System.out.println("..........xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.........................");
+        System.out.println(".........xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx........................");
+        System.out.println(".........xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx........................");
+        System.out.println(".........xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx........................");
+        System.out.println(".........xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.........................");
+        System.out.println("..........xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx..........................");
+        System.out.println("............xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx...........................");
+        System.out.println(".............xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx............................");
+        System.out.println("...............xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx..............................");
+        System.out.println(".................xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx................................");
+        System.out.println("...................xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx..................................");
+        System.out.println("........................xxxxxxxxxxxxxxxxxxxxxxxxxxxx........................................");
+        System.out.println("................................xxxxxxxxxxxxx...............................................");
+        System.out.println("............................................................................................");
+        System.out.println("............................................................................................");
+    }
+}

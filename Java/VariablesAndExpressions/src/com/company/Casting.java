1b0e27551d4ea5903990e4e0076a7503c73468cd
kwame <yamoahkwame@gmail.com>
2020-08-23 19:05:33 +0100

    Create Casting.java

---
+++ Java/VariablesAndExpressions/src/com/company/Casting.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Casting {
+    public static void main(String[] args) {
+        char char1 = 'a';
+        byte byte1 = 127;
+        short short1 = 32767;
+        int int1 = 100;
+        long long1 = 42000000000000L;
+        float float1 = 3.703293f;
+        double double1 = 3.70329370329370;
+        boolean bool1 = true;
+
+        System.out.println("Implicit conversion is when the compiler converts a value to the one it needs on its own");
+        
+
+
+
+        System.out.println("Explicit conversion is when we converts a value to the one the compiler needs," +
+                " some information can be lost");
+
+
+    }
+}
